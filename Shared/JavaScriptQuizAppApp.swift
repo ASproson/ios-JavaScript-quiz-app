b4186c90dddcca0f97e7e012f20830eae18ab604
ASproson <a.sproson@outlook.com>
2022-05-02 18:54:21 +0100

    Initial Commit

---
+++ Shared/JavaScriptQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JavaScriptQuizAppApp.swift
+//  Shared
+//
+//  Created by atlas on 02/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct JavaScriptQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
