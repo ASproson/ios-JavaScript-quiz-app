9353e01ec53ab30f5251f20f52e46c54b75acec3
ASproson <a.sproson@outlook.com>
2022-05-07 15:04:34 +0100

    Adds basic initial quiz format

--- JavaScriptQuiz/Shared/ContentView.swift
+++ JavaScriptQuiz/Shared/ContentView.swift
@@ -8,10 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     var body: some View {
-        Text("Hello, world!")
-            .foregroundColor(.blue)
-            .padding()
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack {
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("How do we log to the console in JavaScript?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Spacer()
+                HStack {
+                    Button(action: {
+                        print("Tapped on choice 1")
+                    }, label: {
+                        Text("console.log()")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on choice 2")
+                    }, label: {
+                        Text("print()")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on choice 3")
+                    }, label: {
+                        Text("log()")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                }
+            }
+        }
+        .foregroundColor(.white)
     }
 }
 
